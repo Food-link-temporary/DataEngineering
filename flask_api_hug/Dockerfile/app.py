0c6607d814c7c6ad8f4b38cc087cf02b94364970
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-20 19:30:23 +0900

    update test app

--- flask_api_hug/Dockerfile/app.py
+++ flask_api_hug/Dockerfile/app.py
@@ -30,6 +30,7 @@ def process_text():
 
 
 def generate_text(input_text: str) -> str:
+    input_text = "다음 식재료를 사용하는 레시피명과 조리과정을 알려주세요.\n"+input_text
     input_ids = TOKENIZER.encode(input_text, return_tensors='pt')
     output = MODEL.generate(input_ids, max_length=150, do_sample=True, temperature=0.3)
     generated_text = TOKENIZER.decode(output[0], skip_special_tokens=True)
@@ -39,13 +40,12 @@ def generate_text(input_text: str) -> str:
 def process_data(data: str) -> Dict[str,str]:
     generated_text = generate_text(data)
 
-    return generated_text
     try:
-        matches = re.search(RECIPE_PATTERN, generated_text, re.DOTALL)
+        # matches = re.search(RECIPE_PATTERN, generated_text, re.DOTALL)
         return {
-            "foodName": matches.group(1).strip(),
-            "ingredients": matches.group(2).strip(),
-            "recipe": matches.group(3).strip()
+            "foodName": "food name", # matches.group(1).strip(),
+            "ingredients": data, # matches.group(2).strip(),
+            "recipe": generated_text.encode("utf-8").decode("utf-8") # matches.group(3).strip()
         }
     except: return {"title":str(), "ingredients":str(), "recipe":str()}
 
