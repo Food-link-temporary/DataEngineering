021b2913b581063a518b6782015cd99ef6049b80
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-20 19:05:43 +0900

    Update app.py

--- flask_api_hug/Dockerfile/app.py
+++ flask_api_hug/Dockerfile/app.py
@@ -18,9 +18,11 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def process_text():
-    print("request 체크: ", request.url)
     
+    return request.url
+
     params = urlparse(request.url).params
+
     print("params 체크:",params)
     params = {key:value for key, value in [param.split('=') for param in params.split('&')]}
 
