3ab46d7ab978d749dfabb70835410d2a93edfaa5
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-20 16:59:48 +0900

    update

--- flask_api_hug/Dockerfile/app.py
+++ flask_api_hug/Dockerfile/app.py
@@ -1,47 +1,50 @@
 from flask import Flask, request, jsonify
+from typing import Dict
+from urllib.parse import urlparse
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+import re
 
+# SEPERATOR = "<SEP>"
+RECIPE_PATTERN = r"Recipe Title:(.*?)Ingredients:(.*?)Instructions:(.*?)$"
 
+# load the pre-trained KoGPT tokenizer
+TOKENIZER = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2')
+
+# load the pre-trained KoGPT model
+MODEL = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
+
+@app.route('/api', methods=['GET'])
 def process_text():
-    text = request.json['text']
+    params = urlparse(request.url).params
+    params = {key:value for key, value in [param.split('=') for param in params.split('&')]}
 
-    # 전달 받은 텍스트를 Spark DataFrame으로 변환
-
+    # 데이터 처리 및 결과 얻기
+    result = process_data(params["ingredients"])
 
     return jsonify(result)
 
 
-def process_data(df):
- 
+def generate_text(input_text: str) -> str:
+    input_ids = TOKENIZER.encode(input_text, return_tensors='pt')
+    output = MODEL.generate(input_ids, max_length=150, do_sample=True, temperature=0.3)
+    generated_text = TOKENIZER.decode(output[0], skip_special_tokens=True)
+    return generated_text
 
-    # load the pre-trained KoGPT tokenizer
-    tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2')
 
-    # load the pre-trained KoGPT model
-    model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+def process_data(data: str) -> Dict[str,str]:
+    generated_text = generate_text(data)
+    try:
+        matches = re.search(RECIPE_PATTERN, generated_text, re.DOTALL)
+        return {
+            "foodName": matches.group(1).strip(),
+            "ingredients": matches.group(2).strip(),
+            "recipe": matches.group(3).strip()
+        }
+    except: return {"title":str(), "ingredients":str(), "recipe":str()}
 
-    input_text = """
-    '코코넛설탕', '토막난 닭', '말린 가지', '멸치다시마육수'
-    위 재료를 사용하는 요리명과 레시피는 다음과 같습니다.
-    """
-
-    # tokenize text
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-
-    # generate text using the model
-    output = model.generate(input_ids, max_length=150, do_sample=True, temperature=0.3)
-
-    # decode generated text
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return result_df.toJSON().collect()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-
