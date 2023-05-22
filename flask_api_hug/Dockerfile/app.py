ddcceb84585febc7f347797480896d79186cd6b0
joron1827 <61901261+joron1827@users.noreply.github.com>
2023-05-22 19:47:01 +0900

    test

--- flask_api_hug/Dockerfile/app.py
+++ flask_api_hug/Dockerfile/app.py
@@ -122,6 +122,9 @@ def load_recipes() -> pd.DataFrame:
     ## 속성명 변환
     df.rename(columns={'recipe_ingredient':'recipeIngredient', 'recipe_instruction':'recipeInstructions'}, inplace=True)
     
+
+    df = df.astype({'recipeIngredient':'str','recipeInstructions':'str'})
+
     df["recipeIngredient"] = df["recipeIngredient"].apply(literal_eval)
     df["recipeInstructions"] = df["recipeInstructions"].apply(literal_eval)
 
